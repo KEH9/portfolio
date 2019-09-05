fbdeebfa96a671c981c80118fae6347a13e0ed62
KEH9 <innokkuz@protonmail.com>
2019-09-06 02:17:15 +0500

    resume header continue

--- src/components/resume/Resume.js
+++ src/components/resume/Resume.js
@@ -50,10 +50,17 @@ class Resume extends Component {
 
         <div className="resume-left">
           <img src={myPhoto} alt="me" className="my-photo" />
+          <div className="personal-info first">Languages:</div>
+          <div className="personal-info">Russian</div>
+          <div className="personal-info end-block">English</div>
+          <div className="personal-info">Birthdate:</div>
+          <div className="personal-info last-info">02 january 1980</div>
+
         </div>
 
         <div className="resume-right">
           <div className="innokentiy-kuznetsov">Innokentiy Kuznetsov</div>
+          <div className="frontend">Front-end software developer</div>
         </div>
 
         <div className="resume-rightzzz">
