c1e12de3793f8ea80cb09e974cb29500c16e312f
KEH9 <innokkuz@protonmail.com>
2019-10-02 17:14:31 +0500

    resume job slide up/down

--- src/components/resume/sections/PastJobs.js
+++ src/components/resume/sections/PastJobs.js
@@ -1,141 +1,195 @@
-/* eslint-disable max-len */
-import React from 'react';
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable class-methods-use-this */
+import React, { Component } from 'react';
 
-export default function PastJobs() {
-  return (
-    <div className="past-jobs">
+export default class PastJobs extends Component {
+  componentDidMount() {
+  }
 
-      <h1>Past Jobs</h1>
-
-      <div className="paragraph">
-        <h3>2016 / present time</h3>
-        <div className="job">
-          Front-end developing studies.
-        </div>
-      </div>
-
-      <div className="paragraph">
-        <h3>2012 / 2018</h3>
-        <div className="job">
-          Private entrepreneur.
-        </div>
-      </div>
-
-      <div className="paragraph">
-        <h3>2010 / 2012</h3>
-        <div>
-          <span className="job">Wurth-Eurasien</span>
-          <span className="position">Position:  Project manager</span>
-          <p>
-            General objectives of the project: delivery, distribution, installation and
-            servicing of the car diagnosis system “WOW (Wurth Online World) Snooper”
-            which includes soft and hardware components.
-          </p>
-          <p>
-            Side project: The creation  geographic information system for internal
-            use only. The purpose of the geoinformational system is to output data
-            of sales and customers from “1C” (Russian Axapta) to the high-resolution
-            map of the cities. The data can be displayed by different ways and colors
-            depend on the parameters. I used OpenOffice.org Basic to create it.
-          </p>
-        </div>
-      </div>
+  toggleView(e) {
+    const jobName = e.target;
+    const jobWrapper = jobName.nextSibling;
+    if (jobWrapper.classList.contains('hide-it')) {
+      jobWrapper.classList.remove('hide-it');
+    } else {
+      jobWrapper.classList.add('hide-it');
+    }
+    if (jobName.classList.contains('opened')) {
+      jobName.classList.remove('opened');
+      jobName.classList.add('closed');
+    } else {
+      jobName.classList.remove('closed');
+      jobName.classList.add('opened');
+    }
+  }
 
 
-      <div className="paragraph">
-        <h3>2009 / 2010</h3>
-        <div>
-          <span className="job">Vitamin-Plus </span>
-          <span className="position">Position:  Specialist for the<br /> software integration</span>
-          <p>
-            - 1C based software integration and improvement (integration with the front office
-            software, writing code for automatic creation of the documents, calculations,
-            printing forms, reports, etc. for a wide range of configurations);
-          </p>
-
-          <p>
-            - The shop equipment installation, adjustment and software integration
-            (trading terminals, magnetic card readers, barcode scanners etc.)
-          </p>
-
-        </div>
-      </div>
+  hideWrapper() {
+  }
 
 
-      <div className="paragraph">
+  render() {
+    return (
 
-        <h3>2007 / 2009</h3>
-        <div>
-          <span className="job">Center for Informational and Finance Technologies (CIFT)</span>
-          <span className="position"> Position: Project manager</span>
+      <div className="past-jobs">
 
-          <p>
-            - Organization of the work group for software integration; negotiations with
-            the customer, negotiations level till the minister of the region;
-            preparation of project and contract documentation ; promotion of the software.
-          </p>
-          <p>
-            During this work I was awarded the silver mark of the Ministry of Finance of
-            the Republic Sakha (Yakutia) for achievements in state finance digitalization.
-          </p>
-          <span className="position">Position: Specialist for the <br />software integration</span>
-          <p>
-            - Setting up and integration of software system (SS) Sapphire.
-            This package is designed to automate the planning and forecasting
-            of the budget in financial institutions of the regions of Russian
-            Federation and municipalities (state finances). Setting up and integration
-            includes: analysis of business processes for budget execution; engineering
-            databases and configuration the SS for storing and processing information,
-            including uploading data from external sources, and building
-            reports (based on analysis and customer requirements);
-            Configuring the SS is carried out both by internal languages, and
-            using VBA for MS Office.
-          </p>
-          <p>
-            - Forming requirements specification for platform developers.
-          </p>
-          <p>
-            - Training of customer employees working with SS.
-          </p>
-        </div>
-      </div>
+        <h1>Past Jobs</h1>
 
-
-      <div className="paragraph">
-
-        <h3>2006 / 2007</h3>
-        <div>
-          <span className="job">The Network of Pharmacies «Atoll-Pharm» /
-            The Network of Pharmacies «36,6»
-          </span>
-          <span className="position">Position: Budgeting specialist</span>
-        </div>
-      </div>
-
-
-      <div className="paragraph">
-        <h3>2002 / 2006</h3>
-        <div>
-          <span className="job">Finance-Budget department of City Ekaterinburg</span>
-          <span className="position"> Position:  Leading Specialist of the <br />
-            division for the budget revenues analysis and forecasting
-          </span>
+        <div className="paragraph">
+          <h3>2016 / present time</h3>
+          <div>
+            <span className="job">Front-end developing studies.</span>
+          </div>
         </div>
 
-      </div>
-
-
-      <div className="paragraph">
-
-        <h3 className="last">2000 / 2001</h3>
-        <div className="last">
-          <span className="job">PG «UralInvestEnergo»</span>
-          <span className="position">Position: Analyst in the Finance Department</span>
+        <div className="paragraph">
+          <h3>2012 / 2018</h3>
+          <div>
+            <span className="job">Private entrepreneur.</span>
+          </div>
         </div>
+
+        <div className="paragraph">
+          <h3>2010 / 2012</h3>
+          <div>
+            <span className="job opened" onClick={this.toggleView.bind(this)}>
+              Wurth-Eurasien
+            </span>
+            <span className="job-wrapper">
+              <span className="position">Position:  Project manager</span>
+              <p>
+              General objectives of the project: delivery, distribution, installation and
+              servicing of the car diagnosis system “WOW (Wurth Online World) Snooper”
+              which includes soft and hardware components.
+              </p>
+              <p>
+              Side project: The creation  geographic information system for internal
+              use only. The purpose of the geoinformational system is to output data
+              of sales and customers from “1C” (Russian Axapta) to the high-resolution
+              map of the cities. The data can be displayed by different ways and colors
+              depend on the parameters. I used OpenOffice.org Basic to create it.
+              </p>
+            </span>
+          </div>
+        </div>
+
+
+        <div className="paragraph">
+          <h3>2009 / 2010</h3>
+          <div>
+            <span className="job opened" onClick={this.toggleView.bind(this)}>
+              Vitamin-Plus
+            </span>
+            <span className="job-wrapper">
+              <span className="position">Position:  Specialist for the<br /> software integration</span>
+              <p>
+                - 1C based software integration and improvement (integration with the front office
+                software, writing code for automatic creation of the documents, calculations,
+                printing forms, reports, etc. for a wide range of configurations);
+              </p>
+
+              <p>
+                - The shop equipment installation, adjustment and software integration
+                (trading terminals, magnetic card readers, barcode scanners etc.)
+              </p>
+            </span>
+
+          </div>
+        </div>
+
+
+        <div className="paragraph">
+
+          <h3>2007 / 2009</h3>
+          <div>
+            <span className="job opened" onClick={this.toggleView.bind(this)}>
+              Center for Informational and Finance Technologies (CIFT)
+            </span>
+
+            <span className="job-wrapper">
+              <span className="position"> Position: Project manager</span>
+
+              <p>
+                - Organization of the work group for software integration; negotiations with
+                the customer, negotiations level till the minister of the region;
+                preparation of project and contract documentation ; promotion of the software.
+              </p>
+              <p>
+                During this work I was awarded the silver mark of the Ministry of Finance of
+                the Republic Sakha (Yakutia) for achievements in state finance digitalization.
+              </p>
+              <span className="position">Position: Specialist for the <br />software integration</span>
+              <p>
+                - Setting up and integration of software system (SS) Sapphire.
+                This package is designed to automate the planning and forecasting
+                of the budget in financial institutions of the regions of Russian
+                Federation and municipalities (state finances). Setting up and integration
+                includes: analysis of business processes for budget execution; engineering
+                databases and configuration the SS for storing and processing information,
+                including uploading data from external sources, and building
+                reports (based on analysis and customer requirements);
+                Configuring the SS is carried out both by internal languages, and
+                using VBA for MS Office.
+              </p>
+              <p>
+                - Forming requirements specification for platform developers.
+              </p>
+              <p>
+                - Training of customer employees working with SS.
+              </p>
+            </span>
+          </div>
+        </div>
+
+
+        <div className="paragraph">
+
+          <h3>2006 / 2007</h3>
+          <div>
+            <span className="job opened" onClick={this.toggleView.bind(this)}>
+              The Network of Pharmacies «Atoll-Pharm» /
+              The Network of Pharmacies «36,6»
+            </span>
+
+            <span className="job-wrapper">
+              <span className="position">Position: Budgeting specialist</span>
+            </span>
+
+          </div>
+        </div>
+
+
+        <div className="paragraph">
+          <h3>2002 / 2006</h3>
+          <div>
+            <span className="job opened" onClick={this.toggleView.bind(this)}>
+              Finance-Budget department of City Ekaterinburg
+            </span>
+            <span className="job-wrapper">
+              <span className="position"> Position:  Leading Specialist of the <br />
+                division for the budget revenues analysis and forecasting
+              </span>
+            </span>
+          </div>
+
+        </div>
+
+
+        <div className="paragraph">
+
+          <h3 className="last">2000 / 2001</h3>
+          <div className="last">
+            <span className="job opened" onClick={this.toggleView.bind(this)}>
+              PG «UralInvestEnergo»
+            </span>
+            <span className="job-wrapper">
+              <span className="position">Position: Analyst in the Finance  <br />Department</span>
+            </span>
+          </div>
+        </div>
+
+
       </div>
-
-
-    </div>
-
-  );
+    );
+  }
 }
